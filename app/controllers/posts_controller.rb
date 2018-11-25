8f6600725bace0ea55c4ad3bde15667813c6b14d
Sanjay Sai <shachsan@hotmail.com>
2018-11-25 00:52:31 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,18 @@ class PostsController < ApplicationController
   end
 
   def new
+    # @post = Post.new
+  end
+
+  def create
     @post = Post.new
+    @post.title = params[:title]
+    @post.description = params[:description]
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      redirect_to 'new'
+    end
   end
 
   # add create method here
